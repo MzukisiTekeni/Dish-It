503c41b14ef1e51eafc83fe64fcc82411823af68
Mzukisi-Tekeni <mzukisitekeni@gmail.com>
2026-09-05 14:51:35 +0200

    create drawables and bottom navigation menu

--- Dish-It/app/build.gradle.kts
+++ Dish-It/app/build.gradle.kts
@@ -42,4 +42,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
